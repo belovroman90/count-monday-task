32d1cfd05181eaa579b8bb679bcc7a2657d3a414
belovroman90 <belov.roman.90@mail.ru>
2022-05-25 20:39:04 +0300

    Do first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import s from './App.module.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export function App() {
+    const [numb, setNumb] = useState<number>(0);
+
+    function onClickInc() {
+        setNumb(numb + 1);
+    }
+
+    function onClickReset() {
+        setNumb(0);
+    }
+
+    const styleCount = {
+        color: numb > 4 ? "red" : "#2A2C36",
+    }
+    const styleInc = numb > 4 ? s.disabled : s.incButton;
+    const styleReset = numb === 0 ? s.disabled : s.resetButton;
+
+    return (
+        <div className={s.app}>
+            <div className={s.container}>
+                <div className={s.count} style={styleCount}>
+                    {numb}
+                </div>
+                <div className={s.buttons}>
+                    <button onClick={onClickInc} className={styleInc} disabled={numb > 4}>inc</button>
+                    <button onClick={onClickReset} className={styleReset} disabled={numb === 0}>reset</button>
+                </div>
+            </div>
+        </div>
+    );
 }
-
-export default App;
